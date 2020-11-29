780dc3033f502c1e6f87c7a7e007adee17d42ccf
South <ota.minami373@gmail.com>
2020-11-29 20:18:29 +0900

    草生やせますように
    Change-Id: I0000000000000000000000000000000000000000

---
+++ src/minami/ohta/com/BuyChestnutRamdon.java
@@ -0,0 +1,19 @@
+package minami.ohta.com;
+
+public class BuyChestnutRamdon {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int chestnut_price = (int)(Math.random()*3 + 1)*100;
+		int chestnut_num = (int)(Math.random()*100 +1);
+
+		System.out.println("いが栗の単価：" +chestnut_price + "円");
+		System.out.println("いが栗の個数：" +chestnut_num+ "個");
+
+		int total =chestnut_price*chestnut_num;
+		System.out.println("合計金額："+total+"円");
+
+		System.out.println("草生えるのか？");
+	}
+
+}
